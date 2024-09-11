e0ba6089a98bba2ccfc0cf5438b29e793b6ef400
Administrator <asimali9596a@gmail.com>
2024-09-11 20:25:56 +0500

    Changes done

--- Lab1/Lab1/Task1.cpp
+++ Lab1/Lab1/Task1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<conio.h>
 #include<string>
 
 using namespace std;
@@ -14,14 +15,29 @@ public:
 	House() {};
 
 
-	void readFromUser() {
-	
+	void InputFromUser() {
+		cout << "Enter the House Owner NAME: ";
+		cin.ignore();
+		cin.getline(Owner, 20);
 		
-			cout << "Enter the House Owner NAME: ";
 
-			cout << "Enter the House Address: ";
-			cout << "Enter the House BedRooms no.: ";
-			cout << "Enter the House Price: ";
+		cout << "Enter the House Address: ";
+		cin.getline(Address, 20);
+
+		cout << "Enter the House BedRooms no.: ";
+		cin >> bedrooms;
+		
+		cout << "Enter the House Price: ";
+		cin >> Price;
+
+		cin.ignore();
+	}
+
+	void OutputData() {
+		cout << Owner << "\t" << Address << "\t" << bedrooms << "\t" << Price << endl;
+	}
+
+	int PriceCheck() {
 
 	}
 };
@@ -31,29 +47,77 @@ public:
 
 
 void main() {
+	//Variables
 	House available[100];
 	int houseNo = 0;
-	cout << " ------ Welcome to Society Management System -------- ";
-	
-	char choice;
+	int choice;
+	bool check = false;
+	//Menu
+
+
 	do
 	{
-		if (houseNo <= 100)
-		{
-			available[houseNo].readFromUser();
-			houseNo++;
-			cout << "Want to enter more data(y/n)";
-			cin >> choice;
+	cout << " ------ Welcome to Society Management System -------- " << endl;
 
+		cout << "---------------------------------------------------------" << endl;
+		cout << "1. Add a House details." << endl;
+		cout << "2. Display All Houses details." << endl;
+		cout << "3.Check Houses details Through queries.";
+		cout << "0.Exit!!" << endl;
+		cout << "______________________________________\n Your Answer: ";
+		cin >> choice;
+		if (choice == 1)
+		{
+			available[houseNo].InputFromUser();
+			houseNo++;
+			check = true;
+			system("cls");
+		}
+		else if (choice == 2)
+		{
+			if (check)
+			{
+			cout << "-----------------------------------" << endl;
+			cout << "Owner\tAddress\tBedRooms\tPrice\n";
+			  for (int i = 0; i < houseNo; i++)
+			  {
+				available[i].OutputData();
+
+			  }
+			}
+			else
+			{
+				cout << "No House data Stored yet??" << endl;
+			}
+		
+		}
+		else if (choice == 3)
+		{
+
+			do
+			{
+				cout << "1. Find houses within your budget" << endl;
+				cout << "2. Discover homes with your preferred number of bedrooms" << endl;
+				cout << "3. Locate the most affordable (Lowest) house" << endl;
+				cout << "4. Find the largest home with the most bedrooms" << endl;
+				cout << "5. Search for properties in a specific city" << endl;
+				cout << "6. Get the best value for price per size" << endl;
+				cout << "7. Exit" << endl;
+
+
+			} while (true);
+		}
+
+		else if (choice == 0)
+		{
+			break;
 		}
 		else
 		{
-			choice == false;
-			cout << "100 house data COMPLETED!!";
+			cout << "Invalid Choice!!" << endl;
 		}
-		
-	} while (choice == 'y');
-
 
 	
+
+	} while (true);
 }
